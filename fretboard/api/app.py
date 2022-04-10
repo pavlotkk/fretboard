2d534b575db83957810ea4037aea9e25af33cea8
Pavlo Tkachuk <pavel.tkachuk@datarobot.com>
2022-04-10 12:05:39 +0300

    add CORS

--- fretboard/api/app.py
+++ fretboard/api/app.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+]
 
 
 def create_app() -> FastAPI():
     app = FastAPI()
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     from fretboard.api.routes import router
 
