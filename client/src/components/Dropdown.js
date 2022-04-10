8777078593f76088ccc9bba2b7c588704c4aa9d5
Pavlo Tkachuk <pavel.tkachuk@datarobot.com>
2022-04-10 11:25:45 +0300

    disable empty dropdown

--- client/src/components/Dropdown.js
+++ client/src/components/Dropdown.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import {convertPrimitiveToOptionObject} from "../shared/converters";
 
-function Dropdown({options, selectedItem = null, onChange = null}) {
+function Dropdown({options, selectedItem = '', onChange = null}) {
     options = options.map(convertPrimitiveToOptionObject);
-    const value = selectedItem === null ? options[0].value : selectedItem;
+    if(!selectedItem){
+        selectedItem = ''
+    }
+
+    const noOptions = options && options.length === 0
+    const firstItem = noOptions ? '' : options[0].value
+    const value = selectedItem.length > 0 ? firstItem : selectedItem;
 
     React.useEffect(() => {
         if (value !== selectedItem) {
@@ -12,7 +18,7 @@ function Dropdown({options, selectedItem = null, onChange = null}) {
     }, [value, selectedItem])
 
     return (
-        <select className="form-select" value={value} onChange={(e) => onChange(e.target.value)}>
+        <select className="form-select" value={value} onChange={(e) => onChange(e.target.value)} disabled={noOptions ? true : null}>
             {options.map(op => {
                 return <option key={op.value} value={op.value}>{op.text}</option>
             })}
